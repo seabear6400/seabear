a096e3fd658779685c49be4fc9fb9582664ddb8d
김성우 <126859882+seabear6400@users.noreply.github.com>
2024-03-19 09:48:52 +0900

    Create 21209_01v3.java

---
+++ 21209_01v3.java
@@ -0,0 +1,19 @@
+package s21209_00;
+
+
+public class S21209_01v3 {
+
+	public static void main(String[] args) {
+		
+		boolean stop= true;
+
+		if(stop) {
+			System.out.println("운행을 중지합니다.");
+		}else {
+			System.out.println("운행을 시작합니다.");
+		}
+		
+
+	}
+
+}
