b35beada005552d45dafd95dcf31257d7e7dd2d3
김성우 <126859882+seabear6400@users.noreply.github.com>
2024-03-19 10:35:21 +0900

    Create S21209_01v4.java

---
+++ S21209_01v4.java
@@ -0,0 +1,17 @@
+package s21209_00;
+
+
+public class S21209_01v4 {
+
+	public static void main(String[] args) {
+	int x=1;
+	int y=2;
+	double result = x/y;
+	
+	 System.out.println("result_0:"+result);
+     System.out.println("result_1:"+ (double)x / y);
+     System.out.println("result_2:"+ x / (double) y);
+     System.out.println("result_3:"+ (double) x / (double) y);
+	}
+
+}
