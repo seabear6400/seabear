9ea097d8659f8f3d8aa253b3749d2da8ebaa118d
김성우 <126859882+seabear6400@users.noreply.github.com>
2024-04-01 12:41:26 +0900

    Create S201209_3.java

---
+++ S201209_3.java
@@ -0,0 +1,24 @@
+package s21209_00;
+
+public class S201209_3 {
+
+	public static void main(String[] args) {
+		
+		int input = 0;
+		
+		if (input == 0) {
+			System.out.println("a : 값은 0 입니다.");
+		}
+		if (input != 0) {
+			System.out.println("a : 값은 0 아닙니다.");
+		}
+		
+		if (input == 0) {
+			System.out.println("b : 값은 0 입니다.");
+		} else {
+			System.out.println("b : 값은 0이 아닙니다.");
+		}
+		
+	}
+
+}
