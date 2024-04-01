98d9ce3285eb639d84604c7f2e2b58ac0eec1ab1
김성우 <126859882+seabear6400@users.noreply.github.com>
2024-04-01 12:07:24 +0900

    Create S21209_09.java

---
+++ S21209_09.java
@@ -0,0 +1,16 @@
+package s21209_00;
+
+public class S21209_09 {
+	
+   public static void main(String[] args)  {
+	   
+	   int intX = 1;
+	   int intY = 1;
+	   int intXunm = ++intX + 10;
+	   int intYunm = intY++ + 10;
+
+	   System.out.println("++intXnum: "+intXunm+" intX: "+intX);
+	   System.out.println("++intYnum: "+intYunm+" intY: "+intY);
+	   
+   }
+}
